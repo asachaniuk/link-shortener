51b62465192fc3e944aa4547524f867ea4fca492
Alex Sachaniuk <asachaniuk@asachaniuk-macbookpro2.roam.corp.google.com>
2020-04-28 13:15:31 -0700

    Add User model, and basic authentication

--- app.js
+++ app.js
@@ -4,13 +4,17 @@ const mongoose = require('mongoose');
 
 const app = express();
 
+app.use('/api/auth', require('./routes/auth.routes'))
+
 const PORT = config.get('port') || 5000;
 
 
 async function start() {
 	try {
 		await mongoose.connect(config.get('mongoUri'), {
-
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+			useCreateIndex: true
 		});
 	} catch (error) {
 		console.log('Server Error: ', error.message);
