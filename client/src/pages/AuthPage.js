6feb49e4ff0ce18c4d9cae0a845b8a673f928a3c
Alex Sachaniuk <asachaniuk@gmail.com>
2020-05-11 15:08:34 -0700

    Add pages and routes

---
+++ client/src/pages/AuthPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const AuthPage = () => {
+	return (
+		<div className="row">
+			<div className="col s6 offset-s3">
+				<h1>Link Shortener</h1>
+			</div>
+		</div>
+	)
+}
